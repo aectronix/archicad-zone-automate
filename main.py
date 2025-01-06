249e5b9f7f67a419d6d4aa9fce44dc61090b722d
Iyur <iyurrr@gmail.com>
2025-01-06 17:37:06 +0200

    Some additions

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ config = {
 		'layer': 'ModelView_LayerName',										# property for single zone layer's name
 		'category': 'Zone_ZoneCategoryCode',								# property for zone category code
 		'area': 'Zone_CalculatedArea',										# property for area measurement
+		'area_m': 'Zone_MeasuredArea',										# property for area measurement
 		# custom:
 		'live': ['ZONES', 'Zone Living'],									# group name & property to define whether zone is live
 		'tag': ['ZONES - Gross', 'Zone Tag'],								# group name & property for zone "tag" (used as unique ID)
 		'area_gross': ['ZONES - Gross', 'Zone Area Gross'],					# group name & property of zone gross area field
+		'area_m_gross': ['ZONES - Gross', 'Zone Area Gross (M)'],			# group name & property of zone gross area field
 		'area_live': ['ZONES - Gross', 'Zone Area Live'],					# group name & property of zone live area field
+		'area_m_live': ['ZONES - Gross', 'Zone Area Live (M)'],					# group name & property of zone live area field
 		'update': ['ZONES - Gross', 'Zone Update'],							# group name & property to store the last refresh date
 	},
-	'round': 1																# rounding value, the number of digits after the point
+	'round': 2																# rounding value, the number of digits after the point
 }
 
 
@@ -70,37 +73,44 @@ class ZoneCommands():
 			layer_propId = self.acc.utilities.GetBuiltInPropertyId(self.conf['props']['layer'])
 			category_propId = self.acc.utilities.GetBuiltInPropertyId(self.conf['props']['category'])
 			area_propId = self.acc.utilities.GetBuiltInPropertyId(self.conf['props']['area'])
+			area_m_propId = self.acc.utilities.GetBuiltInPropertyId(self.conf['props']['area_m'])
 			live_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['live'][0], self.conf['props']['live'][1])
 		except Exception as e:
 			print(f'{timer()}: Some custom properties have been missed!\n')
 			raise			
 
-		zones_prop = self.acc.commands.GetPropertyValuesOfElements(zones, [tag_propId, layer_propId, category_propId, area_propId, live_propId])
+		zones_prop = self.acc.commands.GetPropertyValuesOfElements(zones, [tag_propId, layer_propId, category_propId, area_propId, area_m_propId, live_propId])
 		zones_data = {}
 
 		# extract & store zone data
 		print(f'{timer()}: Processing {len(zones)} items...')
 		for zp in zones_prop:
-			if zp.propertyValues[0].propertyValue.status != 'notAvailable' and (self.conf['layers']['single'] or zp.propertyValues[1].propertyValue.value == self.conf['layers']['single']):
+			if zp.propertyValues[0].propertyValue.status != 'notAvailable' and self.conf['layers']['single'] and zp.propertyValues[1].propertyValue.value == self.conf['layers']['single']:
 				idx = zones_prop.index(zp)
 				# print('> Processing ' + str(idx+1) + '/' + str(len(zones)) + ' (' + str(zones[idx].elementId.guid) + ')...', end='\r')
 				zone_tag = zp.propertyValues[0].propertyValue.value
 				zone_area = round(zp.propertyValues[3].propertyValue.value, self.conf['round'])
-				zone_live = zone_area if zp.propertyValues[4].propertyValue.value else 0
+				zone_area_m = round(zp.propertyValues[4].propertyValue.value, self.conf['round'])
+				zone_live = zone_area if zp.propertyValues[5].propertyValue.value else 0
+				zone_live_m = zone_area_m if zp.propertyValues[5].propertyValue.value else 0
 
 				zones_data[zone_tag] = {
 					'gross': zones_data.get(zone_tag, {}).get('gross', 0) + zone_area,
+					'gross_m': zones_data.get(zone_tag, {}).get('gross_m', 0) + zone_area_m,
 					'live': zones_data.get(zone_tag, {}).get('live', 0) + zone_live,
+					'live_m': zones_data.get(zone_tag, {}).get('live', 0) + zone_live_m,
 					'update': round(start)
 				}
 
 		print(f'{timer()}: Sending gross areas back to zones...')
 		query = []
 		gross_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['area_gross'][0], self.conf['props']['area_gross'][1]).guid
+		gross_m_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['area_m_gross'][0], self.conf['props']['area_m_gross'][1]).guid
 		live_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['area_live'][0], self.conf['props']['area_live'][1]).guid
+		live_m_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['area_m_live'][0], self.conf['props']['area_m_live'][1]).guid
 		update_propId = self.acc.utilities.GetUserDefinedPropertyId(self.conf['props']['update'][0], self.conf['props']['update'][1]).guid
 		for zone in zones_prop:
-			if zone.propertyValues[0].propertyValue.status != 'notAvailable':
+			if zone.propertyValues[0].propertyValue.status != 'notAvailable' and zone.propertyValues[1].propertyValue.value == self.conf['layers']['single']:
 				idx = zones_prop.index(zone)
 				query += [
 					{
@@ -114,11 +124,29 @@ class ZoneCommands():
 		            },
 					{
 		                "elementId": { "guid": zones[idx].elementId.guid },
+		                "propertyId": { "guid": gross_m_propId },
+		                "propertyValue": {
+		                    "type": "area",
+		                    "status": "normal",
+		                    "value": round(zones_data[zone.propertyValues[0].propertyValue.value]['gross_m'], self.conf['round'])
+		                }
+		            },
+					{
+		                "elementId": { "guid": zones[idx].elementId.guid },
 		                "propertyId": { "guid": live_propId },
 		                "propertyValue": {
 		                    "type": "area",
 		                    "status": "normal",
 		                    "value": round(zones_data[zone.propertyValues[0].propertyValue.value]['live'], self.conf['round'])
+		                }
+		            },
+					{
+		                "elementId": { "guid": zones[idx].elementId.guid },
+		                "propertyId": { "guid": live_m_propId },
+		                "propertyValue": {
+		                    "type": "area",
+		                    "status": "normal",
+		                    "value": round(zones_data[zone.propertyValues[0].propertyValue.value]['live_m'], self.conf['round'])
 		                }
 		            },
 					{
